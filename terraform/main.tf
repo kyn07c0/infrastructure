912500f1b881d1cec32b0eb213ea33197ca677cd
std-017-006 <kyn07c0@yandex.ru>
2023-06-01 17:59:31 +0000

    first commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,65 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = ">= 0.87.0"
+    }
+  }
+  
+  # Описание бэкенда хранения состояния
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "terraform-state-yuriy-krivinya"
+    region     = "ru-central1"
+    key        = "terraform.tfstate"
+    access_key = "YCAJEVUhP7h5MRLe-DDDq3_Hx"
+    secret_key = "YCO8ApLBNe0Q9YXNaj4s1da1ANA3w8T8yhW-rFZU"    
 
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
+
+provider "yandex" {
+  cloud_id  = "b1g3jddf4nv5e9okle7p"
+  folder_id = "b1gqiugu40tbisbm38ri"
+  zone      = "ru-central1-a"
+}
+
+resource "yandex_compute_instance" "vm-1" {
+    name = "chapter5-lesson2-std-017-006"
+
+    # Конфигурация ресурсов:
+    # количество процессоров и оперативной памяти
+    resources {
+        cores  = 2
+        memory = 2
+    }
+
+    # Загрузочный диск:
+    # здесь указывается образ операционной системы
+    # для новой виртуальной машины
+    boot_disk {
+        initialize_params {
+            image_id = "fd80f8mhk83hmvp10vh2"
+        }
+    }
+
+    # Сетевой интерфейс:
+    # нужно указать идентификатор подсети, к которой будет подключена ВМ
+    network_interface {
+        subnet_id = "e9b0nup7lqmaiqf045tf"
+        nat       = false
+    }
+
+    # Метаданные машины:
+    # здесь можно указать скрипт, который запустится при создании ВМ
+    # или список SSH-ключей для доступа на ВМ
+    metadata = {
+        ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+    }
+}
+
+output "ip_address" {
+    value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
