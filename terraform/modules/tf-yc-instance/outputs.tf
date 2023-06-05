ce525a22311031258c233cfc5bfbc6b63fda0f9f
std-017-006 <kyn07c0@yandex.ru>
2023-06-05 14:43:55 +0000

    Change outputs.tf

--- terraform/modules/tf-yc-instance/outputs.tf
+++ terraform/modules/tf-yc-instance/outputs.tf
@@ -1,7 +1,7 @@
-output "ip_address" {
+output "internal_ip" {
     value = yandex_compute_instance.vm-1.network_interface.0.ip_address
 }
 
-output "nat_ip_address" {
+output "public_ip" {
     value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
 }
