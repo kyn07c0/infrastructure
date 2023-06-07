fe2e5ed7cd50b37d019bc4e0ad2239736a8c4968
std-017-006 <kyn07c0@yandex.ru>
2023-06-07 18:48:55 +0000

    main.tf and terraform.tfvars

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ module "tf-yc-network" {
 module "tf-yc-instance" {  
   zone = var.zone
   image_id = var.image_id
-  subnet_id = module.tf-yc-network.vpc_subnet["${var.zone}"].subnet_id
+  subnet_id = module.tf-yc-network.vpc_subnet[var.zone].subnet_id
   source = "./modules/tf-yc-instance"
 }
 
